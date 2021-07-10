3e4708d3fafb6416d6029bbaa39ab7c719f4cfa5
justYu2001 <jack4781012@gmail.com>
2021-07-10 19:56:44 +0800

    add var

---
+++ JS Practice/Beginer/var/index.js
@@ -0,0 +1,35 @@
+var a = 0;
+console.log(a);  // 0
+
+var a = 1;
+console.log(a); // 1
+
+function f(){
+    if(true){
+        var a = 1;
+    }
+    console.log(a); // 1
+    let key = 0;
+    switch (key) {
+        case 1:
+            var c = 2;
+            break;
+        default:
+            var c = 5;
+            break;
+    }
+    console.log(c); // 5
+    for(var i = 0; i < 3; i++){
+        a = 1;
+    }
+    console.log(i); // 3
+    while(a++ < 10){
+        var d = 7;
+    }
+    console.log(d); // 7
+}
+
+f();
+
+let e = 1;
+console.log(global.e);
