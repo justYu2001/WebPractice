2e950f919d1feaea630363438023c2be92b2bf78
justYu2001 <jack4781012@gmail.com>
2021-07-12 20:05:21 +0800

    add daily task 1

---
+++ JS Practice/hexschool-vue-summer-camp/daily-task-1/index.js
@@ -0,0 +1,14 @@
+let HexSchool = '六角學院';
+const HexSchoolAge = 5;
+const course = {
+  JavaScript: 2500
+}
+let HexSchoolGroup = {
+  teacher: 'Casper',
+  assistant: 'Ray'
+}
+
+// 後續程式碼，請藉此判斷使用 Let or Const
+HexSchool = '六角學院，帶你學到會的線上課程';
+course.JavaScript = 2000;
+HexSchoolGroup = {};
