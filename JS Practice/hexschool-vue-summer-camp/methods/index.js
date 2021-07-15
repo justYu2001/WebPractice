1038cc2865cb2d60d64766229b00d839c1fa7dc5
justYu2001 <jack4781012@gmail.com>
2021-07-15 18:08:04 +0800

    add methods

---
+++ JS Practice/hexschool-vue-summer-camp/methods/index.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            price: 100,
+            quantity: 10,
+            twd: 1,
+            count: 0,
+        }
+    },
+    methods: {
+        subtotal(){
+            return this.price * this.quantity;
+        }
+    },
+    computed: {
+        subtotalComputed (){
+            return this.price * this.quantity;
+        },
+        jpy: {
+            get () {
+                return parseFloat(Number(this.twd) / 0.278).toFixed(3);
+            },
+            set (val) {
+                this.twd = parseFloat(Number(val) * 0.278).toFixed(3);
+                this.count++;
+            }
+        },
+        usd:{
+            get () {
+                return parseFloat(Number(this.twd) / 28.540).toFixed(3);
+            },
+            set (val) {
+                this.twd = parseFloat(Number(val) * 28.540).toFixed(3);
+            }
+        }
+    },
+}).mount("#app");
