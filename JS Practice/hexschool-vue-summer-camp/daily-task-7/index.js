069f495e4d7a014818b77c78c44f24df2e2c50b6
justYu2001 <jack4781012@gmail.com>
2021-07-20 09:37:43 +0800

    add daily task 7

---
+++ JS Practice/hexschool-vue-summer-camp/daily-task-7/index.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        text1: "Vue 夏令營",
+        text2: "Day 8 每日任務",
+        text3: "d(`･∀･)b"
+      }
+    },
+  })
+   app.mount('#app');
+  
