5416d095fe51870df98a4f69817ac1a4947b9ac7
justYu2001 <jack4781012@gmail.com>
2021-09-13 19:32:59 +0800

    typescript any type

---
+++ typescript-pracitce/any.js
@@ -0,0 +1,7 @@
+var number = 2;
+number = 'yu';
+console.log(number.person.name);
+number.greet();
+var sth;
+sth = 'hi';
+sth = false;
---
+++ typescript-pracitce/any.ts
@@ -0,0 +1,10 @@
+let number: any = 2;
+number = 'yu';
+
+console.log(number.person.name);
+number.greet();
+
+let sth;
+
+sth = 'hi';
+sth = false;
